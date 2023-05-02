83894280e32bc135b649151dbbb5c1cc4c54703f
ssttt016 <ssttt016@gmail.com>
2023-05-02 09:06:27 +0900

    0502

---
+++ src/main/java/com/kbstar/controller/CustController.java
@@ -0,0 +1,66 @@
+package com.kbstar.controller;
+
+import com.kbstar.dto.Cust;
+import com.kbstar.service.CustService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@Controller
+@RequestMapping("/cust")
+public class CustController {
+    @Autowired
+    CustService custService;
+    String dir= "cust/";
+    @RequestMapping("")
+    public String main(Model model){
+        model.addAttribute("center",dir+"center");
+        model.addAttribute("left",dir+"left");
+        //logger.info("------------------------------");
+        Random r = new Random();
+        int inx = r.nextInt(1000)+1;
+        log.info(inx+"");
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public String add(Model model){
+        model.addAttribute("center",dir+"add");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+    @RequestMapping("/get")
+    public String get(Model model, String id) {
+        Cust cust = new Cust(id,"xxx","james");
+        model.addAttribute("gcust",cust);
+        model.addAttribute("center",dir+"get");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+
+    @RequestMapping("/all")
+    public String all(Model model) throws Exception {
+        List<Cust> list = null;
+        try {
+            list = custService.get();
+        }catch(Exception e){
+            throw new Exception("시스템 장애");
+        }
+
+        model.addAttribute("clist",list);
+
+        model.addAttribute("center",dir+"all");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/kbstar/controller/ItemController.java
@@ -0,0 +1,50 @@
+package com.kbstar.controller;
+
+import com.kbstar.dto.Item;
+import com.kbstar.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequestMapping("/item")
+public class ItemController {
+    @Autowired
+    ItemService itemService;
+    String dir= "item/";
+
+    @RequestMapping("")
+    public String main(Model model){
+        model.addAttribute("center",dir+"center");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+    @RequestMapping("/add")
+    public String add(Model model){
+        model.addAttribute("center",dir+"add");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+    @RequestMapping("/all")
+    public String all(Model model) throws Exception {
+        List<Item> list = null;
+        try {
+            list = itemService.get();
+        }catch(Exception e){
+            throw new Exception("시스템 장애:ER0002");
+        }
+        model.addAttribute("allitem",list);
+        model.addAttribute("center",dir+"all");
+        model.addAttribute("left",dir+"left");
+        return "index";
+    }
+}
